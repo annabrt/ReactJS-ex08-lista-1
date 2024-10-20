a8fb4febeae04a114dfd102351c8fe345869b54e
Anna Bertochi <anafbrtc@gmail.com>
2024-10-20 00:17:46 -0300

    Add files via upload
    Exercício 08 da lista 1.

---
+++ ex08l1/src/App.jsx
@@ -0,0 +1,10 @@
+import Online from './components/Online'
+import './App.css'
+
+function App(){
+  return(
+    <Online Online={true}/>
+  )
+}
+
+export default App
---
+++ ex08l1/src/components/Online.jsx
@@ -0,0 +1,5 @@
+function Online(props){
+    return <h1>{props.Online ? "Usuário online" : "Usuário offline"}</h1>
+}
+
+export default Online
